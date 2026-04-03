a0819d03bcfb731f16bf7a4f955e96be32ccd7a7
Alina Gaydarzhi <skvirellsheps@bk.ru>
2026-04-04 00:43:07 +0400

    исправлены замечания ревьюера в BurgerTest: переименованы поля, тесты с несколькими проверками разбиты на отдельные.

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.List;
-
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
@@ -19,13 +17,13 @@ public class BurgerTest {
     private Bun bun;
 
     @Mock
-    private Ingredient ingredient1;
+    private Ingredient firstIngredient;
 
     @Mock
-    private Ingredient ingredient2;
+    private Ingredient secondIngredient;
 
     @Mock
-    private Ingredient ingredient3;
+    private Ingredient thirdIngredient;
 
     @Before
     public void setUp() {
@@ -45,33 +43,37 @@ public class BurgerTest {
     // добавленный ингридиент соответствует
     @Test
     public void addIngredientStoresCorrectIngredientTest() {
-        burger.addIngredient(ingredient1);
-        assertSame(ingredient1, burger.ingredients.get(0));
+        burger.addIngredient(firstIngredient);
+        assertSame(firstIngredient, burger.ingredients.get(0));
     }
 
     // увеличение списка
     @Test
     public void addIngredientIncreasesSizeTest() {
-        burger.addIngredient(ingredient1);
+        burger.addIngredient(firstIngredient);
         assertEquals(1, burger.ingredients.size());
     }
 
     // проверка порядка
     @Test
     public void addIngredientMaintainsOrderTest() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        assertSame(firstIngredient, burger.ingredients.get(0));
+    }
 
-        assertSame(ingredient1, burger.ingredients.get(0));
-        assertSame(ingredient2, burger.ingredients.get(1));
+    @Test
+    public void addIngredientMaintainsOrderSecondTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        assertSame(secondIngredient, burger.ingredients.get(1));
     }
 
     // удаление элемента
     @Test
     public void removeIngredientDecreasesSizeTest() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
         burger.removeIngredient(0);
 
         assertEquals(1, burger.ingredients.size());
@@ -79,49 +81,73 @@ public class BurgerTest {
 
     @Test
     public void removeIngredientShiftsElementsTest() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
         burger.removeIngredient(0);
 
-        assertSame(ingredient2, burger.ingredients.get(0));
+        assertSame(secondIngredient, burger.ingredients.get(0));
     }
 
     @Test
-    public void moveIngredientForwardChangesOrderTest() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-        burger.addIngredient(ingredient3);
-
+    public void moveIngredientForwardFirstPositionTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        burger.addIngredient(thirdIngredient);
         burger.moveIngredient(0, 2);
-
-        List<Ingredient> list = burger.ingredients;
-        assertSame(ingredient2, list.get(0));
-        assertSame(ingredient3, list.get(1));
-        assertSame(ingredient1, list.get(2));
+        assertSame(secondIngredient, burger.ingredients.get(0));
     }
 
     @Test
-    public void moveIngredientBackwardChangesOrderTest() {
-        burger.addIngredient(ingredient1);
-        burger.addIngredient(ingredient2);
-        burger.addIngredient(ingredient3);
+    public void moveIngredientForwardSecondPositionTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        burger.addIngredient(thirdIngredient);
+        burger.moveIngredient(0, 2);
+        assertSame(thirdIngredient, burger.ingredients.get(1));
+    }
 
+    @Test
+    public void moveIngredientForwardThirdPositionTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        burger.addIngredient(thirdIngredient);
+        burger.moveIngredient(0, 2);
+        assertSame(firstIngredient, burger.ingredients.get(2));
+    }
+
+    @Test
+    public void moveIngredientBackwardFirstPositionTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        burger.addIngredient(thirdIngredient);
         burger.moveIngredient(2, 0);
+        assertSame(thirdIngredient, burger.ingredients.get(0));
+    }
 
-        List<Ingredient> list = burger.ingredients;
+    @Test
+    public void moveIngredientBackwardSecondPositionTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        burger.addIngredient(thirdIngredient);
+        burger.moveIngredient(2, 0);
+        assertSame(firstIngredient, burger.ingredients.get(1));
+    }
 
-        assertSame(ingredient3, list.get(0));
-        assertSame(ingredient1, list.get(1));
-        assertSame(ingredient2, list.get(2));
+    @Test
+    public void moveIngredientBackwardThirdPositionTest() {
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+        burger.addIngredient(thirdIngredient);
+        burger.moveIngredient(2, 0);
+        assertSame(secondIngredient, burger.ingredients.get(2));
     }
 
     @Test
     public void getPriceWithIngredientsTest() {
-        when(ingredient1.getPrice()).thenReturn(50f);
+        when(firstIngredient.getPrice()).thenReturn(50f);
 
         burger.setBuns(bun);
-        burger.addIngredient(ingredient1);
+        burger.addIngredient(firstIngredient);
 
         assertEquals(250f, burger.getPrice(), 0.0001);
     }
@@ -135,28 +161,35 @@ public class BurgerTest {
     }
 
     @Test
-    public void getReceiptContainsIngredientDataTest() {
-        when(ingredient1.getName()).thenReturn("Cutlet");
-        when(ingredient1.getType()).thenReturn(IngredientType.FILLING);
-        when(ingredient1.getPrice()).thenReturn(80f);
-
+    public void getReceiptContainsIngredientNameTest() {
+        when(firstIngredient.getName()).thenReturn("Cutlet");
+        when(firstIngredient.getType()).thenReturn(IngredientType.FILLING);
+        when(firstIngredient.getPrice()).thenReturn(80f);
         burger.setBuns(bun);
-        burger.addIngredient(ingredient1);
-
+        burger.addIngredient(firstIngredient);
         String receipt = burger.getReceipt();
-
         assertTrue(receipt.contains("Cutlet"));
+    }
+
+    @Test
+    public void getReceiptContainsIngredientTypeTest() {
+        when(firstIngredient.getName()).thenReturn("Cutlet");
+        when(firstIngredient.getType()).thenReturn(IngredientType.FILLING);
+        when(firstIngredient.getPrice()).thenReturn(80f);
+        burger.setBuns(bun);
+        burger.addIngredient(firstIngredient);
+        String receipt = burger.getReceipt();
         assertTrue(receipt.contains("filling"));
     }
 
     @Test
     public void getReceiptContainsCorrectPriceTest() {
-        when(ingredient1.getPrice()).thenReturn(80f);
-        when(ingredient1.getName()).thenReturn("Cutlet");
-        when(ingredient1.getType()).thenReturn(IngredientType.FILLING);
+        when(firstIngredient.getPrice()).thenReturn(80f);
+        when(firstIngredient.getName()).thenReturn("Cutlet");
+        when(firstIngredient.getType()).thenReturn(IngredientType.FILLING);
 
         burger.setBuns(bun);
-        burger.addIngredient(ingredient1);
+        burger.addIngredient(firstIngredient);
 
         String receipt = burger.getReceipt();
 
